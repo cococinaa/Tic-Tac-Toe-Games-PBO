6af13d8bce3f7e0d920e375b06f5becd57ed5ed1
RichieOng <richieong27092006@gmail.com>
2025-10-27 23:15:34 +0700

    Update Kotak.java

--- PBO Tic Tac Toe/Games/Kotak.java
+++ PBO Tic Tac Toe/Games/Kotak.java
@@ -1,7 +1,7 @@
 import greenfoot.*;
 import java.awt.Graphics2D;
 import java.awt.BasicStroke;
-// Anda harus menggunakan java.awt.Color saat memakai Graphics2D
+
 import java.awt.Color; 
 
 public class Kotak extends Actor
@@ -22,37 +22,29 @@ public class Kotak extends Actor
     public void addedToWorld(World world)
     {
         GreenfootImage image = new GreenfootImage(delta, delta);
-        int d = 30; // Digunakan sebagai ketebalan (thickness)
-        
-        if (ID == 0) // Jika ID adalah 0, gambar simbol X (Merah)
+        int d = 30; 
+        if (ID == 0) 
         {
-            // --- INI ADALAH PERBAIKANNYA ---
-            // Kita gunakan Graphics2D untuk menggambar 'X' secara langsung
+           
             Graphics2D g2 = image.getAwtImage().createGraphics();
-            
-            // Atur ketebalan garis
+        
             g2.setStroke(new BasicStroke(d)); 
-            
-            // Atur warna ke MERAH (HARUS java.awt.Color)
+
             g2.setColor(java.awt.Color.RED); 
 
-            // Garis pertama: dari kiri atas ke kanan bawah
-            // Kita beri padding 'd' agar tebal garis tidak terpotong di tepi
+
             g2.drawLine(d, d, delta - d, delta - d);
             
-            // Garis kedua: dari kanan atas ke kiri bawah
             g2.drawLine(delta - d, d, d, delta - d);
-            
-            // Selesai menggambar
+
             g2.dispose();
-            
-            // Perintah image.setRotation(45); DIBUANG karena itulah penyebab error
+
         }
-        else // Jika ID bukan 0, gambar simbol O (Biru)
+        else 
         {
             Graphics2D g2 = image.getAwtImage().createGraphics();
             g2.setStroke(new BasicStroke(d));
-            g2.setColor(java.awt.Color.BLUE); // Gunakan java.awt.Color
+            g2.setColor(java.awt.Color.BLUE); 
             g2.drawArc(d, d, image.getWidth() - 2 * d, image.getHeight() - 2 * d, 0, 360);
             g2.dispose();
         }
@@ -62,6 +54,6 @@ public class Kotak extends Actor
     
     public void act()
     {
-        // Add your action code here.
+        
     }  
 }
