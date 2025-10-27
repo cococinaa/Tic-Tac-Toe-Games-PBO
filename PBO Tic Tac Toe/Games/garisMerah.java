f02f477a4d514840324cd8a2494c4a3213a812b9
RichieOng <richieong27092006@gmail.com>
2025-10-27 23:20:57 +0700

    Update garisMerah.java

--- PBO Tic Tac Toe/Games/garisMerah.java
+++ PBO Tic Tac Toe/Games/garisMerah.java
@@ -6,19 +6,17 @@ public class garisMerah extends garis
     public void addedToWorld(World world)
     {
         int d = 15;
-        // Membuat gambar dengan lebar World - 10 sel, dan tinggi 15 sel (d)
-        // Ini mewarisi perilaku dari kelas 'garis' tetapi mengganti warna.
+
         GreenfootImage image = new GreenfootImage(world.getWidth() - 10, d);
         
-        // Catatan: Nama kelasnya 'garisMerah', tetapi warnanya diatur menjadi GREEN
+
         image.setColor(Color.GREEN); 
-        image.fill(); // Mengisi seluruh area gambar dengan warna HIJAU
+        image.fill(); 
         setImage(image);
     }
     
-    // Metode act() kosong karena garis ini statis
     public void act()
     {
-        // Add your action code here.
+       
     }
 }
